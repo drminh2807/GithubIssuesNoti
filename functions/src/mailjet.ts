b4f67c3affe28a6d6f8f34426ea2395db5556f24
drminh2807 <drminh2807@gmail.com>
2024-08-29 18:08:15 +0700

    change env

--- functions/src/mailjet.ts
+++ functions/src/mailjet.ts
@@ -4,11 +4,11 @@
  *
  */
 import {Client} from "node-mailjet";
-import {defineSecret} from "firebase-functions/params";
-const publicKey = defineSecret("MJ_APIKEY_PUBLIC");
-const privateKey = defineSecret("MJ_APIKEY_PRIVATE");
 export const sendMail = (email: string, subject: string, content: string) => {
-  const mailjet = Client.apiConnect(publicKey.value(), privateKey.value());
+  const mailjet = Client.apiConnect(
+    process.env.MJ_APIKEY_PUBLIC ?? "",
+    process.env.MJ_APIKEY_PRIVATE ?? ""
+  );
   return mailjet.post("send", {version: "v3.1"}).request({
     Messages: [
       {
